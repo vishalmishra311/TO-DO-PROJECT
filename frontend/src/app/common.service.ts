6db5111540bff4e19a4f107e07e855ea7fb7da80
Vishal Mishra <vishalmishra4400@gmail.com>
2025-10-02 01:52:39 +0530

    Add environment configs and update service for API

--- frontend/src/app/common.service.ts
+++ frontend/src/app/common.service.ts
@@ -1,32 +1,71 @@
+// import { Injectable } from '@angular/core';
+// import { Task } from './models/task.model';
+// import { HttpClient, HttpClientModule } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class CommonService {
+//   constructor(private http: HttpClient) {}
+
+//   public addtask(taskData: Task) {
+//     console.log(taskData, 'hi i am from service ');
+//     return this.http.post('http://localhost:3000/task', taskData);
+//   }
+
+//   public getTaskData(): Observable<Task[]> {
+//     return this.http.get<Task[]>('http://localhost:3000/getTaskDeatils');
+//   }
+
+//   public deleteTask(id: string): Observable<any> {
+//     return this.http.delete(`http://localhost:3000/task/${id}`);
+//   }
+
+//   public getTaskById(id: string) {
+//     return this.http.get(`http://localhost:3000/getTaskbyId/${id}`);
+//   }
+
+//   public upadteTask(id: string, taskdata: Task) {
+//     return this.http.put(`http://localhost:3000/updateTask/${id}`, taskdata);
+//   }
+// }
+
+
+
 import { Injectable } from '@angular/core';
 import { Task } from './models/task.model';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment'; // import environment
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommonService {
+  private apiUrl = environment.apiUrl; // use environment variable
+
   constructor(private http: HttpClient) {}
 
   public addtask(taskData: Task) {
-    console.log(taskData, 'hi i am from service ');
-    return this.http.post('http://localhost:3000/task', taskData);
+    console.log(taskData, 'hi i am from service');
+    return this.http.post(`${this.apiUrl}/task`, taskData);
   }
 
   public getTaskData(): Observable<Task[]> {
-    return this.http.get<Task[]>('http://localhost:3000/getTaskDeatils');
+    return this.http.get<Task[]>(`${this.apiUrl}/getTaskDeatils`);
   }
 
   public deleteTask(id: string): Observable<any> {
-    return this.http.delete(`http://localhost:3000/task/${id}`);
+    return this.http.delete(`${this.apiUrl}/task/${id}`);
   }
 
   public getTaskById(id: string) {
-    return this.http.get(`http://localhost:3000/getTaskbyId/${id}`);
+    return this.http.get(`${this.apiUrl}/getTaskbyId/${id}`);
   }
 
   public upadteTask(id: string, taskdata: Task) {
-    return this.http.put(`http://localhost:3000/updateTask/${id}`, taskdata);
+    return this.http.put(`${this.apiUrl}/updateTask/${id}`, taskdata);
   }
 }
+
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://to-do-project-7.onrender.com'
+};
